266fbb465b4d489db357a2724e79d3e96d5c1a3e
Sufia Bano <124654106+sufirang@users.noreply.github.com>
2023-04-27 11:50:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,54 +1,24 @@
-//your JS code here. If required.
-//your JS code here. If required.
-const res = document.getElementById("output");
+const nameInput = document.getElementById('name');
+const ageInput = document.getElementById('age');
+const submitBtn = document.getElementById('btn');
 
-const promises = [
-  new Promise((resolve) => {
-    const time = Math.floor(Math.random() * 3 + 1) * 1000;
-    setTimeout(() => resolve({ name: "Promise 1", time: time / 1000 }), time);
-  }),
-  new Promise((resolve) => {
-    const time = Math.floor(Math.random() * 3 + 1) * 1000;
-    setTimeout(() => resolve({ name: "Promise 2", time: time / 1000 }), time);
-  }),
-  new Promise((resolve) => {
-    const time = Math.floor(Math.random() * 3 + 1) * 1000;
-    setTimeout(() => resolve({ name: "Promise 3", time: time / 1000 }), time);
-  }),
-];
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault(); 
+  const delayPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Number(ageInput.value) >= 18) {
+        resolve({ name: nameInput.value, age: Number(ageInput.value) });
+      } else {
+        reject({ name: nameInput.value });
+      }
+    }, 4000);
+  });
 
-async function callFns() {
-  const start = new Date();
-  // Use Promise.all to wait for all Promises to resolve
-  res.innerHTML += `
-            <tr id="loading">
-                <td colspan=2>Loading...</td>
-            </tr>
-          `;
-  await Promise.all(promises)
-    .then((results) => {
-      res.innerHTML = ``;
-      // Log the array of results
-      results.forEach((e) => {
-        res.innerHTML += `
-            <tr>
-                <td>${e.name}</td>
-                <td>${e.time}</td>
-            </tr>
-          `;
-      });
+  delayPromise
+    .then((data) => {
+      alert(`Welcome, ${data.name}. You can vote.`);
     })
     .catch((error) => {
-      console.error(error);
-    });
-
-  const end = new Date();
-  const timeInMillis = end - start;
-  res.innerHTML += `
-            <tr>
-                <td>Total</td>
-                <td>${timeInMillis / 1000}</td>
-            </tr>
-          `;
-}
-callFns();
+      alert(`Oh sorry ${error.name}. You aren't old enough.`);
+    });
+});
